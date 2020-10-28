fc1b3976fe26836db73ed292f32caef2bc980672
Nicole Hamilton <Nicole@Warps-MacBook-Air.local>
2020-10-28 08:36:05 -0700

    [js] add new object and element to webpage

--- github.js
+++ github.js
@@ -1,8 +1,20 @@
+const container = document.querySelector(".container");
+
+
 fetch('https://api.github.com/users/NikkiHmltn')
 .then(response => {
     return response.json();
 })
 .then(githubData => {
-    console.log(githubData);
+    const nikkiObj = {
+        name: githubData.name,
+        username: githubData.login,
+        location: githubData.location
+    }
+    console.log(nikkiObj);
+
+    const newElement = document.createElement('p');
+    newElement.textContent = nikkiObj.name;
+    container.appendChild(newElement);
 })
 
